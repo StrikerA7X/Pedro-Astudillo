0047beca1fc7280faf61fa31916274e399510626
PC-16 <PC-16@DESKTOP-8F45B9S>
2022-03-30 01:39:51 -0500

    Cambio mensaje de hola mundo a español.

---
+++ src/main/java/com/mycompany/poo_unidad01_astudillo_pedro/Poo_Unidad01_Astudillo_Pedro.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.poo_unidad01_astudillo_pedro;
+
+/**
+ *
+ * @author PC-16
+ */
+public class Poo_Unidad01_Astudillo_Pedro {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
